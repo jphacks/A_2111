a8eb8bf614326bbb42c87d037cc483435bc808c0
takapiro99 <takapiro99@users.noreply.github.com>
2021-10-25 10:57:58 +0000

    applied prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
@@ -11,10 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 
-
         <a
-
-
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
