ab0add035aacef090378a5ead1f907de03a3b4e5
takapiro99 <bigstitch99@gmail.com>
2021-10-25 19:56:40 +0900

    matrix でみんな commit しちゃうからそりゃだめだ

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -10,7 +10,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+
         <a
+
+
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
