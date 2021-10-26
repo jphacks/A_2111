ac9366b0833956a3288a5d5ad429529039a8a598
takapiro99 <takapiro99@users.noreply.github.com>
2021-10-26 07:43:06 +0000

    applied prettier

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -4,23 +4,11 @@ import { Input, Checkbox, Button, Spacer } from '@chakra-ui/react'
 const Signup = () => {
   return (
     <div className={styles.signupContainer}>
-      <Input
-        placeholder="FullName"
-        size="lg"
-        mt={140}
-        width={"80"}
-        boxShadow="base blue"
-      />
-      <Input
-        placeholder="Password"
-        size="lg"
-        width={"80"}
-        mt={10}
-        boxShadow="base"
-      />
+      <Input placeholder="FullName" size="lg" mt={140} width={'80'} boxShadow="base blue" />
+      <Input placeholder="Password" size="lg" width={'80'} mt={10} boxShadow="base" />
       <Checkbox mt={10}>Terms and conditions</Checkbox>
       <Spacer />
-      <Button mt={20} bg={"blue.200"} size="lg">
+      <Button mt={20} bg={'blue.200'} size="lg">
         Create account
       </Button>
     </div>
