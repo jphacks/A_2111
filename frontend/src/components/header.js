4ee8f586ad08d70182cd8831ee2bd4a85a75972d
usatyo <yu0424.math8128@gmail.com>
2021-10-29 13:06:50 +0900

    色とか変えました

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,5 +1,5 @@
 import "../styles/header.css";
-import Logo from "../assets/MAPLE.png";
+import Logo from "../assets/icon.png";
 import { useState } from "react";
 import { FaGithub } from "react-icons/fa";
 
@@ -19,31 +19,31 @@ const Header = () => {
           headerOpen ? "about_content_show about_content" : "about_content"
         }
       >
-        {/* <div>
-          <p className="MAPLE_title">MAPLE</p>
-          <p className="MAPLE_sub">地図が主役の写真集 MAPLE</p>
+        <div>
+          <p className="MAPLE_title">COCONO</p>
+          <p className="MAPLE_sub"></p>
           <p className="MAPLE_sub2">
-            たくさん投稿してその土地のいいところを伝えよう
+            マスクの自動開閉で快適な毎日を
           </p>
         </div>
         <button
-          style={{color: '#222', background:'#ddd', padding: '10px', borderRadius: '15px'}}
+          style={{color: '#222', background:'#ddd', border: '1px solid black', padding: '10px', borderRadius: '15px'}}
           onClick={() => setHeaderOpen(!headerOpen)}
         >
           <span>close</span>
         </button>
         <div className="footer">
-          <p className="teamName">ガリガリ君</p>
+          <p className="teamName">A-2111 ガリガリ君</p>
           <a
             href="https://github.com/Nano3013/garigarikun"
             target="blank"
             rel="noreferrer"
           >
             <div className="icon1">
-              <FaGithub size={30} color="#eee" />
+              <FaGithub size={30} color="black" />
             </div>
           </a>
-        </div> */}
+        </div>
       </div>
     </>
   );
