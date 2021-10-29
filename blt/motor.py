002515e9d3117249abf791f9c4d2756e81e57dab
koki <38291975+gojiteji@users.noreply.github.com>
2021-10-30 00:40:39 +0900

    Update motor.py

--- blt/motor.py
+++ blt/motor.py
@@ -1,13 +1,16 @@
 import time
 from RpiMotorLib import rpi_dc_lib
+import RPi.GPIO as GPIO
+
 
 # https://github.com/gavinlyonsrepo/RpiMotorLib/blob/master/Documentation/L298N_DC.md
 # forward(n) 又は backward(n)、そして stop(0) で回したり止めたり
 # n は duty(つまり強さ) で、0-100
 # (GPIO(input3) , GPIO(input4) , GPIO(PWM) , freq , verbose, name)
+
 MotorOne = rpi_dc_lib.L298NMDc(20, 21, 12, 100, True, "motor_one")
 
-MotorOne.cleanup(True)
+#MotorOne.cleanup(True)
 
 def close_mask(t=0.5):
     print("closing")
@@ -21,5 +24,4 @@ def open_mask():
     MotorOne.backward(100)  # backward、逆かも
     time.sleep(0.5)
     MotorOne.stop(0)
-
 
