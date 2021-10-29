85c0dffbcf02816b34ccd10dff4b72851000d397
usatyo <usatyo@users.noreply.github.com>
2021-10-29 06:35:14 +0000

    applied prettier

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,10 +1,10 @@
-import "../styles/header.css";
-import Logo from "../assets/icon.png";
-import { useState } from "react";
-import { FaGithub } from "react-icons/fa";
+import '../styles/header.css'
+import Logo from '../assets/icon.png'
+import { useState } from 'react'
+import { FaGithub } from 'react-icons/fa'
 
 const Header = () => {
-  const [headerOpen, setHeaderOpen] = useState(false);
+  const [headerOpen, setHeaderOpen] = useState(false)
   return (
     <>
       <img
@@ -13,35 +13,31 @@ const Header = () => {
         alt="Logo"
         onClick={() => setHeaderOpen(!headerOpen)}
       />
-      <div className={headerOpen ? "headerOpen about" : "about"} />
-      <div
-        className={
-          headerOpen ? "about_content_show about_content" : "about_content"
-        }
-      >
+      <div className={headerOpen ? 'headerOpen about' : 'about'} />
+      <div className={headerOpen ? 'about_content_show about_content' : 'about_content'}>
         <div>
           <p className="MAPLE_title">
             <span className="title_text">COCONO</span>
             <span className="title_text">MASK</span>
           </p>
           <p className="MAPLE_sub"></p>
-          <p className="MAPLE_sub2">
-            マスクの自動開閉で快適な毎日を
-          </p>
+          <p className="MAPLE_sub2">マスクの自動開閉で快適な毎日を</p>
         </div>
         <button
-          style={{color: '#222', background:'#ddd', border: '1px solid black', padding: '10px', borderRadius: '15px'}}
+          style={{
+            color: '#222',
+            background: '#ddd',
+            border: '1px solid black',
+            padding: '10px',
+            borderRadius: '15px'
+          }}
           onClick={() => setHeaderOpen(!headerOpen)}
         >
           <span>close</span>
         </button>
         <div className="footer">
           <p className="teamName">A-2111 ガリガリ君</p>
-          <a
-            href="https://github.com/Nano3013/garigarikun"
-            target="blank"
-            rel="noreferrer"
-          >
+          <a href="https://github.com/Nano3013/garigarikun" target="blank" rel="noreferrer">
             <div className="icon1">
               <FaGithub size={30} color="black" />
             </div>
@@ -49,7 +45,7 @@ const Header = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
