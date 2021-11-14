a913690cde1a39ca8fbee0b850f8944eb6615f5f
takapiro99 <bigstitch99@gmail.com>
2021-11-14 23:03:10 +0900

    splash-screen-boilerplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 import { ErrorBoundary } from 'react-error-boundary'
-import React from 'react'
+import React, { useContext } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import AppContextProvider from './contexts/AppContext'
+import AppContextProvider, { AppContext } from './contexts/AppContext'
 import Home from './pages/Home'
 import Signup from './pages/Signup'
 import { ChakraProvider } from '@chakra-ui/react'
 import ErrorFallback from './pages/ErrorBoundary'
 import NotFound from './pages/NotFound'
+import Splash from './components/Splash'
 
 function App() {
+  const { showSplash } = useContext(AppContext)
+
+  if (showSplash) {
+    return <Splash />
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
