f3ad5792d1fc2fd60c2d3c3facae2ee8660a29fe
takapiro99 <takapiro99@users.noreply.github.com>
2021-11-19 23:33:04 +0000

    applied prettier

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,21 +1,23 @@
-import Triangle from "../components/Triangle"
+import Triangle from '../components/Triangle'
 
 const NotFound = () => {
   return (
     <>
-    <Triangle />
-    <Triangle />
-    <Triangle />
-    <div style={{width: '100vw', height: '100vh', position: 'relative'}}>
-      <p style={{
-        position: 'absolute',
-        left: '50%',
-        top: '50%',
-        transform: 'translate(-50%, -50%)',
-      }}>
-        sorry, not found!
-      </p>
-    </div>
+      <Triangle />
+      <Triangle />
+      <Triangle />
+      <div style={{ width: '100vw', height: '100vh', position: 'relative' }}>
+        <p
+          style={{
+            position: 'absolute',
+            left: '50%',
+            top: '50%',
+            transform: 'translate(-50%, -50%)'
+          }}
+        >
+          sorry, not found!
+        </p>
+      </div>
     </>
   )
 }
